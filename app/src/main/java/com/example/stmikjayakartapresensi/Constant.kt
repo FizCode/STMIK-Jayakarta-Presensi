29e46daca6457aa5aeb8c04d2fe69df4b843a2d5
FizCode1 <abdulhafizemail@gmail.com>
2023-06-20 18:28:31 +0700

    Adding Dependency Injection (DI) AppModule & NetworkModule

---
+++ app/src/main/java/com/example/stmikjayakartapresensi/Constant.kt
@@ -0,0 +1,8 @@
+package com.example.stmikjayakartapresensi
+
+object Constant {
+    object Named {
+        const val BASE_URL = "BASE_URL"
+        const val RETROFIT = "RETROFIT"
+    }
+}
---
+++ app/src/main/java/com/example/stmikjayakartapresensi/di/NetworkModule.kt
@@ -0,0 +1,49 @@
+package com.example.stmikjayakartapresensi.di
+
+import com.example.stmikjayakartapresensi.Constant
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
+import javax.inject.Singleton
+
+class NetworkModule {
+    @Singleton
+    @Provides
+    @Named(Constant.Named.BASE_URL)
+    fun provideBaseUrl(): String = "http://localhost:3000/"
+
+    @Singleton
+    @Provides
+    fun provideHttpLogging(): HttpLoggingInterceptor {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        return httpLoggingInterceptor.apply {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(logging: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    @Named(Constant.Named.RETROFIT)
+    fun provideRetrofit(
+        @Named(Constant.Named.BASE_URL) baseUrl: String,
+        client: OkHttpClient
+    ) : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+}
